9a35aab453aad85348eace832bc67a0a695f3e31
Aftab Shaikh <137868335+aftabshaikhraza@users.noreply.github.com>
2025-05-23 22:25:05 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-account_sid = "ACe886983a3c06d7d69f59907559b6573a"
-auth_token = "28e484ab1600ed34a3fb417a87acba33"
+account_sid = "your_account_sid_here"
+auth_token = "your_auth_token_here"
 from_whatsapp_number = "whatsapp:+14155238886"
-to_whatsapp_number = "whatsapp:+12074080480"
+to_whatsapp_number = "whatsapp:+your_verified_number"
